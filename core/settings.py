354940b7d53216f9c8633b65efde04768554c4a4
tedipermana551 <tedipermana551@gmail.com>
2026-05-25 21:43:51 +0700

    change settings.py

--- core/settings.py
+++ core/settings.py
@@ -137,13 +137,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+_db_url = os.environ.get('DATABASE_URL', '')
+if _db_url:
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.parse(
+            _db_url,
+            conn_max_age=600,
+            ssl_require=True,
+        )
     }
-}
-
+else:
+    # Local SQLite fallback (dev only)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
